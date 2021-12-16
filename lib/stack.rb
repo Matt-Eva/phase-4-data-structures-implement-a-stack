7e4524e5b8997895ec52ebd3ff5ac5d644b39a19
Matt Eva <matteweva@gmail.com>
2021-12-16 14:28:29 -0800

    commit tree 2965d5b156ce36d99a4dc41b6d497412a66f0dcd

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,48 @@
 # your code here
+class Stack
+
+    def initialize(limit = -1)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        if @limit == -1
+            @stack.push(value)
+        elsif @stack.length >= @limit
+            raise "Stack Overflow"
+        else
+            @stack.push(value)
+        end
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack[-1]
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        size == 0
+    end
+
+    def full?
+        size == @limit
+    end
+
+    def search(value)
+        @stack.each_with_index do |element, index|
+            if element == value
+                return size - (index + 1)
+            end
+        end
+        -1
+    end
+
+end
